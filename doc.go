b4de0240c88987c76afcca48cb9da9bf49fdb432
changkong <dz7changkong@qq.com>
2013-08-09 17:37:58 +0800

    update: 完善注释

--- doc.go
+++ doc.go
@@ -2,7 +2,5 @@
 // Use of this source code is governed by a MIT-style
 // license that can be found in the LICENSE file.
 
-// 淘宝开放平台的 go 语言 SDK, 自带web小工具:
-// 可测试基本流程,
-// 还能根据淘宝 ApiMetadata.xml 重新生成SDK源码并编译
+// 淘宝开放平台的 go 语言 SDK, MIT许可。 自带web小工具: 可测试基本流程, 可根据 ApiMetadata.xml 重新生成SDK源码并编译, 保持与淘宝同步
 package open_taobao
