5a8cfbf3cd7ca03ac5b4c617557e4341c1bba38c
yaofangou <mingqi.zhou@buzzinate.com>
2013-11-28 23:15:08 +0800

    增加top授权方式的api调用

--- TaobaoMethodRequest.go
+++ TaobaoMethodRequest.go
@@ -6,6 +6,7 @@ package open_taobao
 
 import (
 	"errors"
+	"time"
 )
 
 type TaobaoMethodRequest struct {
@@ -20,8 +21,14 @@ func (t *TaobaoMethodRequest) GetResponse(accessToken, apiMethodName string, res
 
 	t.SetValue("method", apiMethodName)
 	t.SetValue("format", "json")
-	t.SetValue("access_token", accessToken)
+	t.SetValue("session", accessToken)
 	t.SetValue("v", "2.0")
+	if taobaoConfig.isTop {
+		t.SetValue("timestamp", time.Now().Format("2006-01-02 15:04:05"))
+		t.SetValue("app_key", taobaoConfig.appKey)
+		t.SetValue("sign_method", "md5")
+		t.SetReqUrl("http://gw.api.taobao.com/router/rest")
+	}
 
 	return executeRequest(t, resp, InsecureSkipVerify, DisableCompression)
 }
